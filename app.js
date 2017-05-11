c6856994980a453bbc0ea67cc62bd85d71efd2f0
Jizi Zhang <zjzottawa@gmail.com>
2017-05-11 00:37:05 -0400

    Added express-session

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
+
+// use session for tracking logins
+app.use(session({
+  secret: 'Hello world',
+  resave: true,
+  saveUninitialized: false
+}));
 
 //mongodb connection
 mongoose.connect("mongodb://localhost:27017/bookworm");
