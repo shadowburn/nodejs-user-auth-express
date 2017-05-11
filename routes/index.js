cc9da4bb976a0958a2e739701da0de3a91e7f136
Jizi Zhang <zjzottawa@gmail.com>
2017-05-10 22:05:00 -0400

    Reworked the post method for registration to add user info to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 // Get /reigister
 router.get('/register', function(req, res, next){
@@ -8,7 +9,40 @@ router.get('/register', function(req, res, next){
 
 //Post /register
 router.post('/register', function(req, res, next) {
-  return res.send('User created!');
+  if(req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword){
+      //conform the password
+      if(req.body.password !== req.body.confirmPassword){
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+      //create object with form input
+      var userData = {
+        email: req.body.email,
+        name: req.body.name,
+        favoriteBook: req.body.favoriteBook,
+        password: req.body.password
+      }
+
+      // use schema's 'create' method to insert document into Mongo
+      User.create(userData, function(error, user) {
+        if(error){
+          return next(error);
+        } else {
+          return res.redirect('/profile');
+        }
+      });
+
+
+  } else {
+    var err = new Error('All fields are required.');
+    err.status = 400;
+    return next(err);
+  }
 })
 
 // GET /
